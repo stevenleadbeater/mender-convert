99f0f7df80ac5ff96d530b27c12a423707608d3e
steven <steven.leadbeater@live.co.uk>
2021-08-17 15:16:29 +0100

    Reverting changes made to partitions over misunderstanding probe

--- modules/disk.sh
+++ modules/disk.sh
@@ -145,17 +145,6 @@ disk_create_file_system_from_folder() {
     run_and_log_cmd "sudo umount work/output"
 }
 
-# Print path to the EFI partition filesystem image
-#
-disk_efi_part() {
-    if [ "${MENDER_HAS_EFI_PART}" == "y" ]; then
-        efi_part="work/part-1.fs"
-    else
-        efi_part=""
-    fi
-    echo "${efi_part}"
-}
-
 # Print path to the boot partition filesystem image
 #
 disk_boot_part() {
@@ -177,11 +166,7 @@ disk_boot_part() {
     # boot part was extracted or generated.
     boot_part="work/boot-generated.vfat"
     if [ ! -f ${boot_part} ]; then
-        if [ "${MENDER_HAS_EFI_PART}" == "y" ]; then
-            boot_part="work/part-2.fs"
-        else
-            boot_part="work/part-1.fs"
-        fi
+        boot_part="work/part-1.fs"
     fi
     echo "${boot_part}"
 }
@@ -191,17 +176,9 @@ disk_boot_part() {
 disk_root_part() {
     boot_part="work/boot-generated.vfat"
     if [ ! -f ${boot_part} ]; then
-        if [ "${MENDER_HAS_EFI_PART}" == "y" ]; then
-            root_part="work/part-3.fs"
-        else
-            root_part="work/part-2.fs"
-        fi
+        root_part="work/part-2.fs"
     else
-        if [ "${MENDER_HAS_EFI_PART}" == "y" ]; then
-            root_part="work/part-2.fs"
-        else
-            root_part="work/part-1.fs"
-        fi
+        root_part="work/part-1.fs"
     fi
     echo "${root_part}"
 }
